a44407ecae13e146f08dd87f4aabd64b5ef8252e
Mouz24 <vadim.dontsu@mail.ru>
2023-03-22 00:45:42 +0300

    fix: eslint warnings

--- src/Movies.tsx
+++ src/Movies.tsx
@@ -1,57 +1,51 @@
-import React from 'react'
-import MoviesList from './MoviesConfig'
-import MovieList, { JSONMovie } from './MovieList';
+import React, { Component } from 'react'
+import type { ReactNode } from 'react'
+import type { JSONMovie } from './MovieList'
 import './MovieList'
-import SearchNames from './SearchNames';
+import SearchNames from './SearchNames'
 import Header from './Header'
-import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Link, Outlet, Route, Routes } from 'react-router-dom';
-import AboutUs from './AboutUs';
-import NotFound from './NotFound';
 
-const movieToSearch: string | null = localStorage.getItem('Input');
-localStorage.clear();
+interface MyComponentState {
+  Movies: JSONMovie[]
+}
 
-class Movies extends React.Component<{}, { Movies: JSONMovie[]}> {
-    constructor(props: JSONMovie[]) {
-        super(props)
-        this.state = {
-           Movies: [
-            {
-                "Title" : "The Avengers",
-                "Year" : "2012",
-                "Poster" : "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
-            },
-            {
-                "Title" : "Star Wars: Episode IV - A New Hope",
-                "Year" : "1977",
-                "Poster" : "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
-            },
-            {
-                "Title" : "Spider Man: Lost Cause",
-                "Year" : "2014",
-                "Poster" : "https://m.media-amazon.com/images/M/MV5BYmZkYWRlNWQtOGY0Zi00MWZkLWJiZTktNjRjMDY4MTU2YzAyXkEyXkFqcGdeQXVyMzYzNzc1NjY@._V1_SX300.jpg"
-            },
-            {
-                "Title" : "Fast & Furious",
-                "Year" : "2009",
-                "Poster" : "https://m.media-amazon.com/images/M/MV5BYjQ1ZTMxNzgtZDcxOC00NWY5LTk3ZjAtYzRhMDhlNDZlOWEzXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-            },
-        ]
-        }
-      }
+// eslint-disable-next-line @typescript-eslint/ban-types
+class Movies extends Component<{}, MyComponentState> {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor (props: {}) {
+    super(props)
+    this.state = {
+      Movies: [
+        {
+          Title: 'The Avengers',
+          Year: '2012',
+          Poster: 'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg'
+        },
+        {
+          Title: 'Star Wars: Episode IV - A New Hope',
+          Year: '1977',
+          Poster: 'https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg'
+        },
+        {
+          Title: 'Spider Man: Lost Cause',
+          Year: '2014',
+          Poster: 'https://m.media-amazon.com/images/M/MV5BYmZkYWRlNWQtOGY0Zi00MWZkLWJiZTktNjRjMDY4MTU2YzAyXkEyXkFqcGdeQXVyMzYzNzc1NjY@._V1_SX300.jpg'
+        },
+        {
+          Title: 'Fast & Furious',
+          Year: '2009',
+          Poster: 'https://m.media-amazon.com/images/M/MV5BYjQ1ZTMxNzgtZDcxOC00NWY5LTk3ZjAtYzRhMDhlNDZlOWEzXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg'
+        }]
+    }
+  }
 
-    render() {
+  render (): ReactNode {
     return (
         <>
             <Header/>
             <SearchNames names ={this.state.Movies} />
-            <div>
-            <Outlet/>
-            </div>
-        </>
-  );
-    }
-
+        </>)
+  }
 };
 
-export default Movies;
+export default Movies
